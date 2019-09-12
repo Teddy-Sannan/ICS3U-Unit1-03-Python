59dfadaf26eef2a3f25d4de295914ae9199629cb
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-09-12 02:02:44 +0000

    Hello, World! in python but with style

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: September 11
+# This is the "Hello, World!" program, but with nice styl
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
